08eaafc83102106b3da5d47b765076ef8a43b1ee
Rohit Kumar <rohit9909mr@gmail.com>
2021-12-13 17:20:55 +0530

    final

--- backend-c4/src/index.js
+++ backend-c4/src/index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const {register, login} = require("./controllers/auth.controller")
 const movieController = require("./controllers/movie.controller");
 
+const screenController = require("./controllers/screen.controller");
+const seatController = require("./controllers/seat.controller");
+const showController = require("./controllers/show.controller");
+const threatreController = require("./controllers/threatre.controller");
+
+
 const app = express();
 app.use(express.json());
 
@@ -10,5 +16,10 @@ app.use("/register", register);
 app.use("/login", login)
 
 app.use("/movies", movieController);
+app.use("/screens", screenController);
+app.use("/seats", seatController);
+app.use("/shows", showController);
+app.use("/threatres", threatreController);
+
 
 module.exports = app
